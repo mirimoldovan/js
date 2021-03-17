023ab98aaae055e6889a4bd0378dcf1ee7236d55
Miriam Moldovan <mirybabutia@yahoo.com>
2021-03-18 01:03:22 +0200

    firt commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const Kelvin = 293; // const variable is not going to change it's value
+
+
+var Celsius = Kelvin - 273;
+var Celsius = 20; // the value of Celsius is now 20
+
+var Fahrenheit;
+var Fahrenheit = Celsius * (9/5) + 32;
+//I wrote the formula of converting from fahrenheit to Celsius in order to set the fahrenheit varaible in it’s value
+
+Fahrenheit = Math.floor(Fahrenheit);
+//round the value of fahrenheit down and assign to 'fahrenheit'
+console.log('The temperature is' + 'degrees Fahrenheit');
+
